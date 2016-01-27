4c38e08c111ba9a26374caef8ad75bf3a244582c
SaintKing <dongsai0520@gmail.com>
2016-01-27 18:18:47 +0800

    update

--- proj.android_AdView/src/com/ds/popstar/AppActivity.java
+++ proj.android_AdView/src/com/ds/popstar/AppActivity.java
@@ -236,22 +236,28 @@ public class AppActivity extends Cocos2dxActivity implements AdViewInterface, Ad
 		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
 		oks.setTitle("分享Kylin游戏");
 		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-		oks.setTitleUrl("http://www.isainttech.com/?post=58");
+		oks.setTitleUrl("http://www.isainttech.com/games/apks/CrazyDiamond2016.apk");
 		// text是分享文本，所有平台都需要这个字段
-		oks.setText("疯狂钻石2016,一起来疯狂吧!!! http://www.isainttech.com/?post=58");
+		oks.setText("疯狂钻石2016,一起来疯狂吧!!! http://www.isainttech.com/games/apks/CrazyDiamond2016.apk");
 		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 		//oks.setImagePath("/sdcard/test.jpg");// 确保SDcard下面存在此张图片
 		oks.setImageUrl("http://www.isainttech.com/content/uploadfile/201601/e5221453701250.png");
 		// url仅在微信（包括好友和朋友圈）中使用
-		oks.setUrl("http://www.isainttech.com/?post=58");
+		oks.setUrl("http://www.isainttech.com/games/apks/CrazyDiamond2016.apk");
 		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
-		//oks.setComment("我是测试评论文本");
+		oks.setComment("棒棒哒!");
 		// site是分享此内容的网站名称，仅在QQ空间使用
 		oks.setSite("Kylin's Games");
 		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
-		oks.setSiteUrl("http://www.isainttech.com/?post=58");
+		oks.setSiteUrl("http://www.isainttech.com/games/apks/CrazyDiamond2016.apk");
 		// 启动分享GUI
 		oks.show(actInstance);
+		
+        String eventStr = "Event_Share";
+        
+        MobclickAgent.onEvent(actInstance,eventStr);
+        
+        Log.i(LOG_NAME, "Event : " + eventStr);
 	}
 
 	public void showDialog(String title, String msg)
@@ -366,6 +372,12 @@ public class AppActivity extends Cocos2dxActivity implements AdViewInterface, Ad
 	{
 		// TODO Auto-generated method stub
 		//Log.i(LOG_NAME, "onClickAd");
+		
+        String eventStr = "Event_Clicked";
+        
+        MobclickAgent.onEvent(actInstance,eventStr);
+        
+        Log.i(LOG_NAME, "Event : " + eventStr);
 	}
 
 	@Override
@@ -407,6 +419,11 @@ public class AppActivity extends Cocos2dxActivity implements AdViewInterface, Ad
 	{
 		// TODO Auto-generated method stub
 		//Log.i(LOG_NAME, "onDisplayAd");
+        String eventStr = "Event_Show";
+        
+        MobclickAgent.onEvent(actInstance,eventStr);
+        
+        Log.i(LOG_NAME, "Event : " + eventStr);
 	}
 
 	@Override
@@ -421,6 +438,11 @@ public class AppActivity extends Cocos2dxActivity implements AdViewInterface, Ad
 	{
 		// TODO Auto-generated method stub
 		//Toast.makeText(actInstance, "ReceivedAd", Toast.LENGTH_SHORT).show();
+        String eventStr = "Event_Instl_Received";
+        
+        MobclickAgent.onEvent(actInstance,eventStr);
+        
+        Log.i(LOG_NAME, "Event : " + eventStr);
 	}
 
 	@Override
@@ -428,6 +450,11 @@ public class AppActivity extends Cocos2dxActivity implements AdViewInterface, Ad
 	{
 		// TODO Auto-generated method stub
 		//Toast.makeText(actInstance, "onReceiveAdFailed", Toast.LENGTH_SHORT).show();
+        String eventStr = "Event_Instl_ReceFailed";
+        
+        MobclickAgent.onEvent(actInstance,eventStr);
+        
+        Log.i(LOG_NAME, "Event : " + eventStr);
 	}
 
 }
